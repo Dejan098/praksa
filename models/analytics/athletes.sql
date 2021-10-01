5327929781e128b3c25b6b401510e702b72506fd
Dejan098 <dvornicdejan08@gmail.com>
2021-10-01 15:26:59 +0200

    first commit

--- models/analytics/athletes.sql
+++ models/analytics/athletes.sql
@@ -6,7 +6,7 @@ with medals as (
            sum(case when medal_type = 'Bronze Medal' then 1 else 0 end) as bronze_medal_count
 	from {{ source('imported_data', 'medals') }}
 	 
-	group by athlete_name
+	group by athlete_name,country,athlete_sex,discipline
 
 
 )
@@ -15,22 +15,22 @@ with medals as (
 
 select
   initcap(name) as name,
-  short_name as short_name,
+  initcap(short_name) as short_name,
   gender as gender,
   birth_date as birth_date,
-  birth_place as birth_place,
+  initcap(birth_place) as birth_place,
   birth_country as birth_country,
   country as country,
   country_code as country_code,
   discipline as discipline,
   discipline_code as discipline_code,
-  residence_place as residence_place,
-  substring("height_m/ft", 1, 4) as height_m,
+  initcap(residence_place) as residence_place,
+  left("height_m/ft", strpos("height_m/ft", '/') - 1)::float as height_m,
   url as url,
-  DATE_PART('year', '2012-01-01'::date) - DATE_PART('year', "birth_date"::date) as age,
-  medals.gold_medal_count as gold_medal_count,
-  medals.silver_medal_count as silver_medal_count,
-  medals.bronze_medal_count as bronze_medal_count
+  date_part('year', '2012-01-01'::date) - date_part('year', "birth_date"::date) as age,
+  coalesce(medals.gold_medal_count,0) as gold_medal_count,
+  coalesce(medals.silver_medal_count,0) as silver_medal_count,
+  coalesce(medals.bronze_medal_count,0) as bronze_medal_count
 
 from {{ source('imported_data', 'athletes') }}
 
